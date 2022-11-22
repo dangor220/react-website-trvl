2c7c58cc11c1bc1871e3cf69496c2540d3246732
Danil Gordeev <dan.gor220@yandex.ru>
2022-11-23 01:34:24 +0300

    feat: add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Osya</h1> 
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+     
+    </>
   );
 }
 
